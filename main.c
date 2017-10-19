df0f8296ae3d94a2d2d48738b4e9d384734edde2
LongmonHou <longmonhou@boyaa.com>
2017-10-19 18:25:01 +0800

    update

--- main.c
+++ main.c
@@ -6,42 +6,56 @@
 
 extern char **environ;
 
-static char **os_main_argv = NULL;
-static char *os_main_argv_end = NULL;
 
-void cli_set_proc_title_init(int argc, char **argv, char **envp);
-
-void cli_set_proc_title( const char *fmt, ...);
+void cli_set_proc_title(int argc, char *argv[], const char *title);
 
 int main( int argc, char *argv[] ){
-    cli_set_proc_title_init(argc,argv,environ);
-    cli_set_proc_title("title:%s asddd\n", "aaaaaa");
-    printf("os_main_argv:%p\n", os_main_argv);
-    printf("os_main_argv_end:%p\n", os_main_argv_end);
+    cli_set_proc_title(argc, argv, "cli_test title");
+    int i;
+    for( i = 0; argv[i]; i++ ) {
+        printf("argv[%d](%p):%s\n", i, argv[i], argv[i]);
+    }
+    for( i = 0; environ[i]; i++ ) {
+        printf("environ[%d](%p):%s\n", i, environ[i], environ[i]);
+    }
+    for(;;){}
 }
 
-void cli_set_proc_title( const char *fmt, ...) {
-    va_list vp;
+void cli_set_proc_title(int argc, char *argv[], const char *title) {
+    int i;
     char *p;
-    int i;
-    va_start(vp, fmt);
-    vsprintf(p, fmt, vp);
-    va_end(vp);
-
-}
-
-void cli_set_proc_title_init(int argc, char *argv[], char **envp) {
-    int i;
-    for( i = 0; envp[i]; i++ ) continue;
-    if( i > 0 ) {
-        os_main_argv_end = envp[i-1]+strlen(envp[i-1])+1;
-    } else {
-        os_main_argv_end = argv[argc-1]+strlen(argv[argc-1])+1;
+    size_t size;
+    char **tmpargv;
+    size_t argv_size = 0;
+    for( i = 0; environ[i]; i++ ){
+        size += strlen(environ[i])+1;
     }
-    for( i = 0; envp[i]; i++ ){
-        environ[i] = (char **)calloc( sizeof(char), strlen(envp[i])*sizeof(char));
-        strcpy(environ[i], envp[i]);
+    for(i = 0; argv[i]; i++ ) {
+        argv_size += strlen(argv[i])+1;
     }
-    environ[i] = NULL;
-    os_main_argv = argv;
+    tmpargv = (char**)calloc(sizeof(char), sizeof(char)*argv_size+strlen(title)+1);
+    p = (char*)malloc(size);
+    char *last_argv = argv[0];
+    for( i = 0; i < argc; i++ ){
+        if( last_argv == argv[i] ) {
+            last_argv = argv[i]+strlen(argv[i])+1;
+        }
+    }
+    for( i = 0; environ[i]; i++ ) {
+        if( last_argv == environ[i] ) {
+            last_argv = environ[i]+strlen(environ[i])+1;
+            size = strlen(environ[i])+1;
+            memcpy(p, environ[i], size);
+            environ[i] = (char*)p;
+            p += size;
+        }
+    }
+    last_argv--;
+    tmpargv[0] = strdup(title);
+    argv[0] = tmpargv[0];
+    for( i = 1; argv[i]; i++ ) {
+        tmpargv[i] = strdup(argv[i]);
+        argv[i] = tmpargv[i];
+    }
+
 }
